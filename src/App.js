8e6c4f6dcf0a3c1968ac917e9e0bb89351d4d0eb
CatsAreMLG <joshuadgon@gmail.com>
2021-02-02 18:57:10 -0600

    Display web3 account

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Web3 from 'web3'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends Component {
+
+  constructor(props){
+    super(props)
+    this.state= {
+      account: ''
+    }
+  }
+
+  async componentDidMount() {
+    await this.loadWeb3()
+    await this.loadBlockchainData()
+  }
+
+  async loadWeb3() {
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum)
+      await window.ethereum.enable()
+    }
+    else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider)
+    }
+    else {
+      window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!')
+    }
+  }
+
+  async loadBlockchainData() {
+    const web3 = new Web3(Web3.givenProvider || "http://localhost:8545")
+    const network = await web3.eth.net.getNetworkType()
+    const accounts = await web3.eth.getAccounts()
+    this.setState({account: accounts[0]})
+  }
+
+  render (){
+    return <div className="container">
+      <h1>Hello, World!</h1>
+      <p>Your account: {this.state.account}</p>
     </div>
-  );
+  }
 }
 
 export default App;
